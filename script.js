e3cb9f56a29b787f576fdd2c8d419e28deaa5a0b
TBAH-IOT <tranthanhquanid69@gmail.com>
2025-01-17 11:05:24 +0700

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-function toggleSection(sectionId) {
-    var sections = document.querySelectorAll('main .section');
+function showSection(sectionId) {
+    var sections = document.querySelectorAll('.content-section');
     sections.forEach(function(section) {
-        if (section.id === sectionId) {
-            section.style.display = section.style.display === 'none' ? 'block' : 'none';
-        } else {
-            section.style.display = 'none';
-        }
+        section.style.display = 'none';
     });
+    document.getElementById(sectionId).style.display = 'block';
 }
